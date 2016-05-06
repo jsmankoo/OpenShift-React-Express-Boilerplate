6eb85b2958061e6a1cd3e1b48746d75ab5cb0bed
Jai Singh Mankoo <jsmankoo@outlook.com>
2016-05-06 05:35:27 -0400

    testing production

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ const config = require(`./webpack.dev`);
 
 // Webpack Dev Server for Hot module reloading
 // Comment it out during production
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true,
-  stats: {colors: true}
-}).listen(3001, `localhost`, function (err, result) {
-  if (err) {
-    return console.log(err);
-  }
-
-  console.log(`Listening at http://localhost:3001/`);
-});
+// new WebpackDevServer(webpack(config), {
+//   publicPath: config.output.publicPath,
+//   hot: true,
+//   historyApiFallback: true,
+//   stats: {colors: true}
+// }).listen(3001, `localhost`, function (err, result) {
+//   if (err) {
+//     return console.log(err);
+//   }
+//
+//   console.log(`Listening at http://localhost:3001/`);
+// });
 
 
 // Init express
